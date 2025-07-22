ac6d1cfb5aa73261e75fec606b3f33ff00879317
TotoCode <totocodefr@duck.com>
2025-07-22 17:35:16 +0200

    je sais plus ce que j'ai rajouté

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,27 @@
-import { Events, PresenceUpdateStatus } from "discord.js";
+import { ActivityType, Events, PresenceUpdateStatus } from "discord.js";
 import { initServer } from "../server.js";
+
+const statuses = [
+  { name: 'vos modmails.', type: ActivityType.Listening },
+  { name: 'est mal programmé.', type: ActivityType.Custom },
+  { name: 'version 0.7.8', type: ActivityType.Custom },
+];
+
+let index = 0;
 
 export default {
     name: Events.ClientReady,
     once: true,
     execute(client) {
         console.log(`Connecté en tant que ${client.user.tag}`);
-        client.user.setPresence({ activities: [{ name: 'version 0.1.5' }], status: PresenceUpdateStatus.Online });
+        setInterval(() => {
+            const presence = statuses[index % statuses.length];
+            client.user.setPresence({
+                activities: [presence],
+                status: 'online'
+            });
+            index++;
+        }, 30_000)
         initServer(process.env.PORT || 8080, client);
     }
 }
