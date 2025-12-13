168a99a62a5b363241c6950dbc58f72d8c8492b0
TotoCode <totocodefr@duck.com>
2025-12-13 09:29:41 +0100

    fix: crash lorsque qqn rejoint

--- events/join.js
+++ events/join.js
@@ -1,12 +1,12 @@
 import { Events } from "discord.js";
-import objectConfig from '../objectConfig.js'
+import objectConfig from '../objectConfig.js';
 
 export default {
     name: Events.GuildMemberAdd,
     async execute(member) {
         await member.send({
-            embeds: [ objectConfig.interactiveDM.serverGuide ],
-            components: objectConfig.interactiveDM.serverGuideRow
-        })
+            embeds: [objectConfig.interactiveDM.serverGuide.embed],
+            components: objectConfig.interactiveDM.serverGuide.row
+        });
     }
-}
+};
